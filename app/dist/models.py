198ee76c7ed4ca48ce685b4df7ba91ca5629b5c9
Scot Matson <sjpmatson@gmail.com>
2016-11-11 09:02:16 +0000

    Added prototype model for users table, appears db communications are established

--- app/dist/models.py
+++ app/dist/models.py
@@ -1,17 +1,29 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import INET, TIMESTAMP
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    firstName = db.Column(db.String(20))
+    lastName = db.Column(db.String(20))
+    password = db.Column(db.String(64))
+    lastLogin = db.Column(TIMESTAMP)
+    ip = db.Column(INET)
 
+    def __init__(self, username, first_name, last_name, password, last_login, ip):
+        self.username = username
+        self.firstName = first_name.title()
+        self.lastName = last_name.title()
+        self.set_password(password) 
+        self.lastLogin = last_login
+        self.ip = ip
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
-
-
-    # pbkdf2:sha256
-
-
-
-    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
