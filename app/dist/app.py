22320718255d4a2dcc96b9d8b61208b9e9ae69cc
Scot Matson <sjpmatson@gmail.com>
2016-11-13 17:09:27 +0000

    Created an admin template and also started documenting

--- app/dist/app.py
+++ app/dist/app.py
@@ -10,18 +10,35 @@ db.init_app(app)
 
 @app.route('/')
 def index():
+    '''
+    The primary user interface for carrying out
+    activities once they have been logged in to 
+    their account.
+    '''
     return render_template('index.jinja2')
 
 @app.route('/login')
 def login():
+    '''
+    A view for entering authorization credentials to access
+    a user account. New users will have the option to
+    create a new account.
+    '''
     return render_template('login.jinja2')
 
 @app.route('/register')
 def register():
+    '''
+    A standard account registration view.
+    '''
     return render_template('register.jinja2')
 
 @app.route('/confirmation', methods=['POST'])
 def confirmation():
+    '''
+    A confirmation view to notify a newly registered user the
+    outcome of their new account registration. 
+    '''
     new_user = User(
         request.form['username'],
         request.form['first-name'],
@@ -33,6 +50,15 @@ def confirmation():
     db.session.add(new_user)
     db.session.commit()
     return render_template('confirmation.jinja2')
+
+@app.route('/admin')
+def admin():
+    '''
+    An administrative view reserved for authorized users.
+    Provides heightened level of access for viewing
+    and interacting with the postgresql database.
+    '''
+    return render_template('admin.jinja2')
 
 
 
