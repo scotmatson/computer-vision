9ce764c3b9c6c1aab189588b729a3c560e5c3851
Scot Matson <sjpmatson@gmail.com>
2016-12-05 04:33:15 +0000

    Added brief statement regarding lib

--- dist/static/js/register.bundle.js
+++ dist/static/js/register.bundle.js
@@ -3,7 +3,7 @@ webpackJsonp([4],[
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var ReactDOM = __webpack_require__(34);
+	var ReactDom = __webpack_require__(34);
 	__webpack_require__(172);
 
 	class Register extends React.Component {
@@ -41,53 +41,95 @@ webpackJsonp([4],[
 
 	    render() {
 	        return React.createElement(
-	            'form',
-	            { action: 'confirmation', method: 'POST', id: 'registration-form', onSubmit: this.handleSubmit },
+	            'div',
+	            { className: 'form-page__wrapper' },
 	            React.createElement(
-	                'label',
-	                { htmlFor: 'username' },
-	                'Username:'
-	            ),
-	            React.createElement('input', { type: 'text',
-	                value: this.state.username,
-	                name: 'username',
-	                id: 'username',
-	                onChange: this.handleUsernameChange }),
-	            React.createElement(
-	                'label',
-	                { htmlFor: 'first-name' },
-	                'First Name'
-	            ),
-	            React.createElement('input', { type: 'text',
-	                value: this.state.firstName,
-	                name: 'first-name',
-	                id: 'first-name',
-	                onChange: this.handleFirstNameChange }),
-	            React.createElement(
-	                'label',
-	                { htmlFor: 'last-name' },
-	                'Last Name'
-	            ),
-	            React.createElement('input', { type: 'text',
-	                value: this.state.lastName,
-	                name: 'last-name',
-	                id: 'last-name',
-	                onChange: this.handleLastNameChange }),
-	            React.createElement(
-	                'label',
-	                { htmlFor: 'password' },
-	                'Password'
-	            ),
-	            React.createElement('input', { type: 'text',
-	                value: this.state.password,
-	                name: 'password',
-	                id: 'password',
-	                onChange: this.handlePasswordChange }),
-	            React.createElement('input', { type: 'submit', value: 'Submit' })
+	                'div',
+	                { className: 'form-page__form-wrapper' },
+	                React.createElement(
+	                    'div',
+	                    { className: 'form-page__form-header' },
+	                    React.createElement(
+	                        'h2',
+	                        { className: 'form-page__form-heading' },
+	                        'Register'
+	                    )
+	                ),
+	                React.createElement(
+	                    'form',
+	                    { action: 'confirmation', method: 'POST', id: 'registration-form', onSubmit: this.handleSubmit },
+	                    React.createElement(
+	                        'div',
+	                        { className: 'form__field-wrapper' },
+	                        React.createElement(
+	                            'label',
+	                            { className: 'form__field-label', htmlFor: 'username' },
+	                            'Username'
+	                        ),
+	                        React.createElement('input', { className: 'form__field-input',
+	                            input: true, type: 'text',
+	                            value: this.state.username,
+	                            name: 'username',
+	                            id: 'username',
+	                            onChange: this.handleUsernameChange })
+	                    ),
+	                    React.createElement(
+	                        'div',
+	                        { className: 'form__field-wrapper' },
+	                        React.createElement(
+	                            'label',
+	                            { className: 'form__field-label', htmlFor: 'first-name' },
+	                            'First Name'
+	                        ),
+	                        React.createElement('input', { className: 'form__field-input',
+	                            input: true, type: 'text',
+	                            value: this.state.firstName,
+	                            name: 'first-name',
+	                            id: 'first-name',
+	                            onChange: this.handleFirstNameChange })
+	                    ),
+	                    React.createElement(
+	                        'div',
+	                        { className: 'form__field-wrapper' },
+	                        React.createElement(
+	                            'label',
+	                            { className: 'form__field-label', htmlFor: 'username' },
+	                            'Last Name'
+	                        ),
+	                        React.createElement('input', { className: 'form__field-input',
+	                            input: true, type: 'text',
+	                            value: this.state.lastName,
+	                            name: 'last-name',
+	                            id: 'last-name',
+	                            onChange: this.handleLastNameChange })
+	                    ),
+	                    React.createElement(
+	                        'div',
+	                        { className: 'form__field-wrapper' },
+	                        React.createElement(
+	                            'label',
+	                            { className: 'form__field-label', htmlFor: 'password' },
+	                            'Password'
+	                        ),
+	                        React.createElement('input', { className: 'form__field-input',
+	                            input: true, type: 'text',
+	                            value: this.state.password,
+	                            name: 'password',
+	                            id: 'password',
+	                            onChange: this.handlePasswordChange })
+	                    ),
+	                    React.createElement(
+	                        'div',
+	                        { className: 'form__submit-btn-wrapper' },
+	                        React.createElement('input', { type: 'submit', value: 'Submit' })
+	                    )
+	                )
+	            )
 	        );
 	    }
 	}
 	ReactDOM.render(React.createElement(Register, null), document.getElementById('react-register-container'));
+	eactDOM.render(React.createElement(Register, null), document.getElementById('react-register-container'));
 
 /***/ }
 ]);
